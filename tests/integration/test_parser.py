35f338dd0bd7039990d97622ba4e4019d2190a4c
James Lee <james.lee@rea-group.com>
2020-10-19 00:53:45 +1100

    Add more integration test with all possible single byte characters

--- tests/integration/test_parser.py
+++ tests/integration/test_parser.py
@@ -51,6 +51,78 @@ aa aa       cc  c    d
         os.remove(input_filepath.replace('.txt', '.csv'))
         os.remove(spec_filepath)
 
+
+    @pytest.fixture
+    def all_characters_content_and_spec_and_expected_rows(self):
+        spec = {
+            "ColumnNames": [
+                "a",
+                "b",
+                "c",
+                "d"
+            ],
+            "Offsets": [
+                "10",
+                "5",
+                "5",
+                "5"
+            ],
+            "FixedWidthEncoding": "windows-1252",
+            "IncludeHeader": "True",
+            "DelimitedEncoding": "utf-8"
+        }
+
+        def generate_bytes_chars(row_characters, offsets):
+            offset, *rest_offsets = offsets
+            column_characters, rest_characters = row_characters[:int(offset)], row_characters[int(offset):]
+            yield ''.join(column_characters)
+
+            if rest_characters:
+                yield from generate_bytes_chars(rest_characters, rest_offsets)
+
+        def generate_content(charaters, offsets):
+            row_characters, rest = charaters[:25], charaters[25:]
+
+            yield generate_bytes_chars(row_characters, offsets), ''.join(row_characters)
+
+            if rest:
+                yield from generate_content(rest, offsets)
+
+        # windows-1252 encoding is single byte character. The range is from 0x00 - 0xFF.
+        # It does not support 5 codes - 0x81, 0x8d, 0x8f, 0x90, 0x9d
+        all_possible_charaters = [
+            bytes([i]).decode('windows-1252')
+            for i in range(256)[1:] if not i in [129, 141, 143, 144, 157]]
+
+        expected_rows, rows = zip(*[
+            (list(row_characters), content)
+            for row_characters, content in generate_content(all_possible_charaters, spec['Offsets'])])
+
+        header = ["         a", "    b", "    c", "    d"]
+
+        content = '\n'.join([''.join(header), *rows])
+
+        return content, spec, [header, *expected_rows]
+
+    @pytest.fixture
+    def file_with_all_characters_and_spec_file(self, all_characters_content_and_spec_and_expected_rows):
+        content, spec, expected_rows = all_characters_content_and_spec_and_expected_rows
+
+        spec_filepath = './test-spec.json'
+        with open(spec_filepath, 'w') as jsonfile:
+            json.dump(spec, jsonfile)
+
+        input_filepath = './test-fixed-width-file.txt'
+        with open(input_filepath, 'w', encoding=spec['FixedWidthEncoding']) as file:
+            file.write(content)
+
+        yield input_filepath, spec_filepath, expected_rows
+
+        os.remove(input_filepath)
+        os.remove(input_filepath.replace('.txt', '.csv'))
+        os.remove(spec_filepath)
+
+
     def test_run_given_a_fixed_width_file_and_spec_file_generates_a_csv_file(self,
                                                                              file_with_simple_characters_and_spec_file):
         input_file, spec_file = file_with_simple_characters_and_spec_file
@@ -69,3 +141,16 @@ aa aa       cc  c    d
         ]
 
         assert rows == expected_content
+
+
+    def test_run_given_a_file_with_all_characters_generates_a_valid_csv(self,
+                                                                        file_with_all_characters_and_spec_file):
+        input_filepath, spec_filepath, expected_rows = file_with_all_characters_and_spec_file
+
+        run(input_filepath, spec_filepath)
+
+        with open(input_filepath.replace('.txt', '.csv'), 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            rows = list(reader)
+
+        assert rows == expected_rows
