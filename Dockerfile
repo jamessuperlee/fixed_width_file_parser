7fe81346b4541c6e7fc935d7330ace6edd599cca
James Lee <james.lee@rea-group.com>
2020-10-18 22:34:41 +1100

    Dockerise lint and test

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM            python:3.8.1-slim as base
+
+WORKDIR         /opt/code/fwf_parser/
+
+RUN             pip install -U pip
+
+COPY            requirements_dev.txt requirements_dev.txt
+RUN             pip install -r requirements_dev.txt
---
+++ scripts/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+die () { echo "$1" >&2; exit 1; }
+
+hash docker || { die "docker is not installed. Exiting..."; }
+
+dockerOptions="-v """$(pwd):/opt/code/fwf_parser""""
+tag="fwf_parser-test"
+dockerRun="docker run $dockerOptions -t $tag"
+
+echo "Building the image..."
+if ! docker build -f Dockerfile -t $tag .; then
+    die "Failed to build the image"
+fi
+
+echo "Running the linter..."
+if ! $dockerRun pylint fwf_parser tests; then
+
+    die "Failed linting"
+fi
+
+echo "Running the tests..."
+if ! $dockerRun pytest --cov fwf_parser; then
+
+    die "Failed tests"
+fi
